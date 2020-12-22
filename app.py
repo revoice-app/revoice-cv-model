9f7cdf5adfd8ccbdc0000ef6deb03ae9c77adcc8
tarunnsingh <tarunnsingh2019@gmail.com>
2020-12-23 00:51:43 +0530

    Updated encodebytes to b64...

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def get_response_image(image_path):
     pil_img = Image.open(image_path, mode='r') # reads the PIL image
     byte_arr = io.BytesIO()
     pil_img.save(byte_arr, format='PNG') # convert the PIL image to byte array
-    encoded_img = base64.encodebytes(byte_arr.getvalue()).decode('ascii') # encode as base64
+    encoded_img = base64.b64encode(byte_arr.getvalue()).decode('utf-8')# encode as base64
     return encoded_img
 
 
